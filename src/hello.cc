663dedeb528bfc5acd513d2d18d50e52a5132f6e
wayling <waylingII@gmail.com>
2021-07-21 14:23:45 +0800

    Add abseil test code
    first commit

---
+++ src/hello.cc
@@ -0,0 +1,9 @@
+#include "absl.h"
+
+absl::flat_hash_map<int, std::string> fn_return_flat_hash_map(void) {
+  absl::flat_hash_map<int, std::string> numbers = {
+      {1, "one"}, {2, "two"}, {3, "three"}};
+  return numbers;
+}
+
+int main() { fn_return_flat_hash_map(); }
