8b2ef2f6605a6155e6b8bbd0c31a38bb87a4bee8
wayling <waylingII@gmail.com>
2021-07-21 15:39:13 +0800

    Add example code

--- src/hello.cc
+++ src/hello.cc
@@ -1,3 +1,4 @@
+#include <iostream>
 #include "absl.h"
 
 absl::flat_hash_map<int, std::string> fn_return_flat_hash_map(void) {
@@ -6,4 +7,12 @@ absl::flat_hash_map<int, std::string> fn_return_flat_hash_map(void) {
   return numbers;
 }
 
-int main() { fn_return_flat_hash_map(); }
+int main() {
+  absl::flat_hash_map<int, std::string> tmp = fn_return_flat_hash_map();
+  std::cout << tmp[1] << std::endl;
+  std::cout << tmp.size() << std::endl;
+  std::cout << tmp.capacity() << std::endl;
+  std::cout << tmp.max_size() << std::endl;
+  std::cout << tmp.bucket_count() << std::endl;
+  std::cout << tmp.load_factor() << std::endl;
+}
